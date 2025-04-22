4266ed0476d9d7f3fbb91e3e165bc9dec6c8f252
ST10277747Kiara <164771350+ST10277747Kiara@users.noreply.github.com>
2025-04-22 13:10:52 +0200

    Third commit: Added CybersecurityQuestions class to manage cybersecurity-related question and answer pairs

--- ST10277747_Prog_POEPart1/CybersecurityQuestions.cs
+++ ST10277747_Prog_POEPart1/CybersecurityQuestions.cs
@@ -6,7 +6,16 @@ using System.Threading.Tasks;
 
 namespace ST10277747_Prog_POEPart1
 {
-    internal class CybersecurityQuestions
+    public class CybersecurityQuestion
     {
+        public string Question { get; set; }
+        public string Answer { get; set; }
+
+        // Constructor
+        public CybersecurityQuestion(string question, string answer)
+        {
+            Question = question;
+            Answer = answer;
+        }
     }
 }
