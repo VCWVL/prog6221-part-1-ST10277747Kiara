9b79ddc9b002ec1a2dcf19d118bc2fb0df80f5a8
ST10277747Kiara <164771350+ST10277747Kiara@users.noreply.github.com>
2025-04-22 12:22:44 +0200

    Add project files.

---
+++ ST10277747_Prog_POEPart1/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ST10277747_Prog_POEPart1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
